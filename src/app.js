aea4f21f7ef3a8994094718d835cd77bc205f566
tyq <1135409448@qq.com>
2018-08-14 23:19:53 -0400

    add scss

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+let x = <div>hello</div>;
+
+class TestComponent extends React.ComponentP {
+    constructor(){
+        super(props);
+        this.state = {
+            name: 'test'
+        }
+
+    }
+    render() {
+        return <h1>hello2 {this.state.name}</h1>
+    }
+}
+
+
+ReactDOM.render(
+    <div></div>
+)
