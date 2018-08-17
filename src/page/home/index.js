2b317bdcdf78d730d2c57a166b2698e479f380bb
tyq <1135409448@qq.com>
2018-08-16 23:44:53 -0400

    add count util service

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -3,10 +3,36 @@ import './index.scss';
 // import { LayOut } from 'component/layout/index.js';
 import { PageTitle } from 'component/pagetitle/index.js';
 import { Link } from 'react-router-dom';
+import Util from 'util/util.js';
+import UtilService from 'service/userService.js';
+
+
+const _utilService = new UtilService();
+
+const _util = new Util();
 
 export class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userCount: '-',
+            productCount: '-',
+            orderCount: '-'
+        }
+    }
+
+    componentDidMount() {
+        this.loadCount();
+    }
+
+    loadCount() {
+        _utilService.getCount().then(
+            (res) => {
+                this.setState(res);
+            }, (err) => {
+                _util.sendErr(err);
+            }
+        )
     }
 
     render() {
@@ -17,17 +43,20 @@ export class Home extends React.Component {
                     <PageTitle title="Dashboard" />
                     <div className="row">
                         <div className="col-md-4">
-                            <Link to="/user" className="color-box ">
+                            <Link to="/user" className="color-box yellow">
+                                <p className="count">{this.state.userCount}</p>
                                 <p className="desc">user</p>
                             </Link>
                         </div>
                         <div className="col-md-4">
-                            <Link to="/product">
+                            <Link to="/product" className="color-box green">
+                                <p className="count">1254</p>
                                 <p className="desc">product</p>
                             </Link>
                         </div>
                         <div className="col-md-4">
-                            <Link to="/order">
+                            <Link to="/order" className="color-box blue">
+                                <p className="count">15342</p>
                                 <p className="desc">order</p>
                             </Link>
                         </div>
